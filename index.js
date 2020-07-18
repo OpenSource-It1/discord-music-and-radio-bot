5e5d39f2d39ffed0c6924c94109dddc4bc77e4ee
BenjaminJForster <68151270+BenjaminJForster@users.noreply.github.com>
2020-07-18 20:27:50 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client({ disableEveryone: true });
+require('./utils/functions')(client);
+client.config = require('./botconfig.json');
+const db = require('quick.db');
+client.categories = [];
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.queue = new Discord.Collection();
+require('./functions')(client);
+
+client.login(process.env.token).then((token) => {
+  console.log("Succesfully logged in");
+  client.user.setStatus('online');
+  
+}).catch((e) => {
+  console.log(e)
+})
