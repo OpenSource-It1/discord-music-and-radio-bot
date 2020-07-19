418c02e750756d4095b674f22e12206963ed0880
BenjaminJForster <68151270+BenjaminJForster@users.noreply.github.com>
2020-07-19 22:57:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ disableEveryone: true });
 require('./utils/functions')(client);
 client.config = require('./botconfig.json');
-const db = require('quick.db');
 client.categories = [];
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.queue = new Discord.Collection();
 require('./functions')(client);
+const fs = require("fs");
 
 client.login(process.env.token).then((token) => {
   console.log("Succesfully logged in.");
