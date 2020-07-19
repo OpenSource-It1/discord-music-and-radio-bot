7a500d826df0e8f3737f9e42a40ca9c70f497544
BenjaminJForster <68151270+BenjaminJForster@users.noreply.github.com>
2020-07-19 23:19:34 +0100

    Update ready.js

--- Events/Bot/ready.js
+++ Events/Bot/ready.js
@@ -1,7 +1,6 @@
 module.exports = async (client) => {
     
   console.log(`${client.user.username} is online on ${client.guilds.cache.size} servers!`);
-  client.user.setActivity('*help');
   client.user.setStatus('online');
     
 }
