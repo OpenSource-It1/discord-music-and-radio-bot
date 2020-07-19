1efa50663c2ab34883cdc7d9a2f64b2a2c9ce184
BenjaminJForster <68151270+BenjaminJForster@users.noreply.github.com>
2020-07-19 23:13:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client({ disableEveryone: true });
-require('./utils/functions')(client);
-client.config = require('./botconfig.json');
+require('./Utilities/functions')(client);
+client.config = require('./Config/botconfig.json');
 client.categories = [];
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
